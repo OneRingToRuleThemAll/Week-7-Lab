cdeb897b36c8331ea5fab7cc3e7808f40d3cb995
Jason W <williams.jasonmd@gmail.com>
2015-08-18 21:45:15 -0500

    finished project

--- Week7LabPin/Controllers/InterestsController.cs
+++ Week7LabPin/Controllers/InterestsController.cs
@@ -6,10 +6,12 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Week7LabPin.Migrations;
 using Week7LabPin.Models;
 
 namespace Week7LabPin.Controllers
 {
+    [Authorize]
     public class InterestsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
@@ -30,6 +32,18 @@ namespace Week7LabPin.Controllers
         {
             var list = db.Interests.ToList().Select(p => new { p.URL, p.Link, p.Comment }).ToList();
             return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult New(string ImageUrl, string Link, string Comment)
+        {
+            var currentUser = db.Users.Where(x => x.UserName == User.Identity.Name).FirstOrDefault();
+            var physicalByteImage = Configuration.GetImageByteArray(ImageUrl);
+            var newinterest = new Interest() { URL = Link, Comment = Comment, Poster = currentUser, Image = physicalByteImage };
+            db.Interests.Add(newinterest);
+            db.SaveChanges();
+            
+
+            return Json(new { Link = newinterest.Link });
         }
     }
 }
